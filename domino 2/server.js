aef63e88d41c442686b9e63607aa9dae26056333
javiloria <34045912+javiloria@users.noreply.github.com>
2019-10-28 19:46:00 -0400

    agregando atributo en las partidas para manejar las fichhas jugadas

--- domino 2/server.js
+++ domino 2/server.js
@@ -113,9 +113,10 @@ class Partida{
     this.turno_jugador= 0;
     this.jugador1 = {ip : "" , fichas : [] };
     this.jugador2 = {ip : "" , fichas : [] };
-    this.estatus= "espera";
+    this.estatus= "ESPERA";
     this.jugadoresEnEstaPartida = 0;
     this.ganador = "";
+    this.fichas_jugadas=[];
     this.fichas_partida=["0:0","0:1","0:2","0:3","0:4","0:5","0:6",
     "1:0","1:1","1:2","1:3","1:4","1:5","1:6",
     "2:0","2:1","2:2","2:3","2:4","2:5","2:6",
